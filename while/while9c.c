92aaa68a5118e234330158507aeb655cf4dc9e22
Miguel <perfectact.2023@gmail.com>
2026-03-20 12:16:53 +0000

    salvar

--- while/while9c.c
+++ while/while9c.c
@@ -1,24 +1,26 @@
-//while de primo
+//while de senha tentativa 3 x
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-   
-    int senha_cadastrada=1234;
-    int senha, tentativa=0;
+  
+    int senha, tentativa=1;
     
    printf("Digite a sua senha: ");
    scanf("%d", &senha);
    
-   while(senha==! senha_cadastrada){
-       printf("Senha invalida\ntente novamente: ");
-        scanf("%d", &senha);
-        tentativa+=1;
-        if(tentativa==3){
-            printf("numero de tentativas ultrapassada");
-            break;
+   while(senha!= 1234){
+       if(tentativa==3){
+           printf("numero de tentativas ultrapassada");
+           break;
         }
+        printf("Senha invalida\ntente novamente: ");
+         scanf("%d", &senha);
+         tentativa+=1;
    }
-   printf("Bem-Vindo");
+   if(senha ==1234){
+
+       printf("Bem-Vindo");
+   }
     
     
     
